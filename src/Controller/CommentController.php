7b77e670643d9f81ed3a32591508e2e7b346d7df
Lam Teng <lamteng@hotmail.com>
2020-11-28 23:15:57 +0000

    updated some js

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,17 +1,30 @@
 <?php
+
 namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
 class CommentController extends AbstractController
 {
-
-    public function commentVote($id, $directoin)
+    /**
+     * @Route("/comments/{id}/vote/{direction<up|down>}")
+     */
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
-        if ($direction === 'up'){
+        // todo - use id to query the database
+
+        // use real logic here to save this to the database
+        if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
-        }else{
+        } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
-        return new JsonResponse(['votes' => $currentVoteController]);
+
+        return $this->json(['votes' => $currentVoteCount]);
     }
-    
 }
