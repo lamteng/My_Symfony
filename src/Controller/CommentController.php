9c3310b86ef370c25ed7b84c88660fc83b20079c
Lam Teng <lamteng@hotmail.com>
2020-11-29 09:01:46 +0000

    added two recipes for api setup

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -10,18 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction<up|down>}")
+     * @Route("/comments/{id}/vote/{direction}")
      */
-    public function commentVote($id, $direction, LoggerInterface $logger)
+    public function commentVote($id, $direction)
     {
         // todo - use id to query the database
 
         // use real logic here to save this to the database
         if ($direction === 'up') {
-            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
-            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
